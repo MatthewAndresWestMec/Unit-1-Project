f1aa16b3d41d14c9a8018ce3e77d751e56079a7f
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2023-01-16 00:04:16 -0700

    fixed media queries kinda

--- assignment.js
+++ assignment.js
@@ -14,6 +14,7 @@ class list {
         this.legalAge = false;
         this.cost = 300;
         this.time = 0;
+        this.age=0;
     }
 }
 
@@ -44,12 +45,13 @@ function addToList() {
         let checkMap = ids.map(x =>{return document.getElementById(x).value})
         let temp = new list(checkMap[0],checkMap[1],checkMap[2],checkMap[3],checkMap[4],checkMap[5],checkMap[6],checkMap[7],meal,extra,count);
         count++;
-        if((Date.now() - Date.parse(temp.dob)) >= (24*60*60*1000)) temp.legalAge = true
+        if((Date.now() - Date.parse(temp.dob)) >= (21*365*24*60*60*1000)) temp.canDrink = true
         let extraArray = temp.extras.split(" ");
         if(extraArray.length >= 1) extraArray.pop()
         console.log(extraArray)
         temp.time = (Date.parse(temp.returnDate) - Date.parse(temp.departureDate))/(24*60*60*1000)
         temp.cost += Number(temp.bags*20)+ Number(extraArray.length*10)
+        temp.age = (Date.now() - Date.parse(temp.dob))/(24*60*60*1000*365)
         arr.push(temp);
         for(let i = 0;i<ids.length;i++) {document.getElementById(ids[i]).value = "";}
         idCheck = 0;
@@ -72,6 +74,9 @@ function display(display){
     }
     document.getElementById("mealInfo").value = arr[display].meal
     document.getElementById("extraInfo").value = arr[display].extras
+    document.getElementById("age").value = arr[display].age
+    document.getElementById("trip").value = arr[display].time
+    document.getElementById("cost").value = arr[display].cost
     for(let i = 0;i<arr.length;i++)
     {
         if(document.getElementById("firstNameInfo").value == arr[i].firstName && document.getElementById("lastNameInfo").value == arr[i].lastName) 
@@ -107,6 +112,24 @@ function print() {
 var personFake1 = new list("Misato", "Katsuragi", "1986-12-08", "Los Angeles", "Alberta", "2023-02-13", "2023-05-16","1","vegetarian", "Extra Room ", "1")
 var personFake2 = new list("Micheal", "Reeves", "2001-11-03", "Washingtin", "Charleston", "2024-01-13", "2024-01-16","3","chicken", "Extra Room Headphones 2nd Serving ", "2")
 var personFake3 = new list("Lain", "Iwakura", "2007-7-06", "Charleston", "Tuscon", "2022-01-13", "2022-01-16","2","fish", "Extra Room Window Seat ", "3")
+personFake1.time = (Date.parse(personFake1.returnDate) - Date.parse(personFake1.departureDate))/(24*60*60*1000)
+personFake1.age = (Date.now() - Date.parse(personFake1.dob))/(24*60*60*1000*365)
+let extraArray = personFake1.extras.split(" ");
+if(extraArray.length >= 1) extraArray.pop()
+personFake1.cost += Number(personFake1.bags*20)+ Number(extraArray.length*10)
+
+personFake2.time = (Date.parse(personFake2.returnDate) - Date.parse(personFake2.departureDate))/(24*60*60*1000)
+personFake2.age = (Date.now() - Date.parse(personFake2.dob))/(24*60*60*1000*365)
+extraArray = personFake2.extras.split(" ");
+if(extraArray.length >= 1) extraArray.pop()
+personFake2.cost += Number(personFake2.bags*20)+ Number(extraArray.length*10)
+
+personFake3.time = (Date.parse(personFake3.returnDate) - Date.parse(personFake3.departureDate))/(24*60*60*1000)
+personFake3.age = (Date.now() - Date.parse(personFake3.dob))/(24*60*60*1000*365)
+extraArray = personFake3.extras.split(" ");
+if(extraArray.length >= 1) extraArray.pop()
+personFake3.cost += Number(personFake3.bags*20)+ Number(extraArray.length*10)
+
 var arr = []
 var ids =["firstName", "lastName", "dob", "depart","arrival","departureDate","returnDate","bags"]
 var arr2 =["firstNameInfo", "lastNameInfo", "dobInfo", "departInfo","arrivalInfo","departureDateInfo","returnDateInfo","bagsInfo"]
